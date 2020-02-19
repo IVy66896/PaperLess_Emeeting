f5ca62ef50e4ad53ebc92931d30bff116b50555c
peter0817 <59242083+peter0817@users.noreply.github.com>
2020-02-19 01:13:42 +0000

    Create PemMemoInfos.cs

---
+++ PemMemoInfos.cs
@@ -0,0 +1,38 @@
+public class PemMemoInfos
+{
+	public double canvasWidth
+	{
+		get;
+		set;
+	}
+
+	public double canvasHeight
+	{
+		get;
+		set;
+	}
+
+	public double strokeAlpha
+	{
+		get;
+		set;
+	}
+
+	public string points
+	{
+		get;
+		set;
+	}
+
+	public string strokeColor
+	{
+		get;
+		set;
+	}
+
+	public double strokeWidth
+	{
+		get;
+		set;
+	}
+}
